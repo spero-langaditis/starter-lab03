9e5c906714a8d797f3862f2a119413726cd7c535
spero <slangaditis94@hotmail.com>
2015-09-24 17:11:26 -0700

    Fixed first link

---
+++ application/controllers/First.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of File
+ *
+ * @author spero
+ */
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $text = $this->quotes->first();
+        $this->data = array_merge($this->data, $text);
+        $this->render();
+    }
+}
